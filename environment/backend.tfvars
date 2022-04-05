bccc2801e367427b1df12e1d72a591b59ee55bfe
Hedi23 <ajimi-hedi@hotmail.fr>
2022-04-05 11:05:13 +0200

    test

--- environment/backend.tfvars
+++ environment/backend.tfvars
@@ -1,8 +1,8 @@
 terraform {
     backend "s3" {
         encrypt = false
-        #bucket = "casestudy-tf-bucket-s3"
-        #dynamodb_table = "casestudy-tf-state-lock-dynamo"
+        # bucket = "casestudy-tf-bucket-s3"
+        # dynamodb_table = "casestudy-tf-state-lock-dynamo"
         bucket = "tests3pipeline"
         dynamodb_table = "dynamodbtest"
         key = "./terra/terraform.tfstate.d/dev/terraform.tfstate"
--- terra/main.tf
+++ terra/main.tf
@@ -7,10 +7,10 @@ terraform {
   }
   backend "s3" {
     encrypt = false
-    bucket = "casestudy-tf-bucket-s3"
-    dynamodb_table = "casestudy-tf-state-lock-dynamo"
-    # bucket = "tests3pipeline"
-    # dynamodb_table = "dynamodbtest"
+    # bucket = "casestudy-tf-bucket-s3"
+    # dynamodb_table = "casestudy-tf-state-lock-dynamo"
+    bucket = "tests3pipeline"
+    dynamodb_table = "dynamodbtest"
     key = "./terra/terraform.tfstate.d/dev/terraform.tfstate"
     region = "us-east-1"
   }
